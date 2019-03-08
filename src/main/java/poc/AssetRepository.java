0a5b83ba9c8331541cdebbe40205351dde5f38a7
jgarciaherrero <javigh73@gmail.com>
2019-03-08 16:31:36 +0100

    Añadido soporte a CORS

--- src/main/java/poc/AssetRepository.java
+++ src/main/java/poc/AssetRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "assets", path = "assets")
 public interface AssetRepository extends MongoRepository<Asset, String> {
 
--- src/main/java/poc/EntityRepository.java
+++ src/main/java/poc/EntityRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "entities", path = "entities")
 public interface EntityRepository extends MongoRepository<Entity, String> {
 
